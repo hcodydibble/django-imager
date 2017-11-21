4802dcae747baee9ca41bc79d8d4ff15b9ea714c
Hewlette Dibble <hcodydibble@gmail.com>
2017-11-20 16:27:59 -0800

    imagerprofile auto created whenever a new user is

--- imager_profile/models.py
+++ imager_profile/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 SLR = 'SLR'
 R_FIND = 'Range finder'
@@ -41,3 +43,13 @@ class ImagerProfile(models.Model):
 
     def active():
         return [user.username for user in User.objects.all() if user.is_active]
+
+@receiver(post_save, sender=User)
+def create_user_imager_profile(sender, instance, created, **kwargs):
+    if created:
+        ImagerProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_imager_profile(sender, instance, **kwargs):
+    instance.profile.save()
