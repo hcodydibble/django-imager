92ad55139504a9a13142ad9255ac5e9cff1387d3
Hewlette Dibble <hcodydibble@gmail.com>
2017-11-29 17:26:21 -0800

    links working

--- imager_images/views.py
+++ imager_images/views.py
@@ -20,12 +20,12 @@ class AlbumView(ListView):
     model = Album
     exclude = []
 
-    # def get_context_data(self, pk):
-    #     super(AlbumView, self).get_context_data()
-    #     user = User.objects.get(username='hcodydibble')
-    #     album = user.album.get(id=2)
-    #     image = album.photo_set.get(id=pk)
-    #     return {'image': image}
+    def get_context_data(self, **kwargs):
+        queryset = Album.objects.filter(id=self.kwargs['pk'])
+        album = queryset.get()
+        photos = album.photo_set.all()
+        return {'photos': photos}
+        
 
 
 
