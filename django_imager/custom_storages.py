01c16f64439cc0504f9295b60ef2c8005c0c6d16
Chris Closser <chrisclosser1@gmail.com>
2017-12-12 09:00:19 -0800

    changed custom storages for testing

--- django_imager/custom_storages.py
+++ django_imager/custom_storages.py
@@ -1,14 +1,16 @@
 """Custom storage classes for static and media files."""
-from __future__ import absolute_import
-from django.conf import settings
-from storages.backends.s3boto import S3BotoStorage
 
-class StaticStorage(S3BotoStorage):
-    """Custom storage class for static files."""
+if not DEBUG:
+    from __future__ import absolute_import
+    from django.conf import settings
+    from storages.backends.s3boto import S3BotoStorage
 
-    location = settings.STATICFILES_LOCATION
+    class StaticStorage(S3BotoStorage):
+        """Custom storage class for static files."""
 
-class MediaStorage(S3BotoStorage):
-    """Custom storage class for media files."""
+        location = settings.STATICFILES_LOCATION
 
-    location = settings.MEDIAFILES_LOCATION
+    class MediaStorage(S3BotoStorage):
+        """Custom storage class for media files."""
+
+        location = settings.MEDIAFILES_LOCATION
