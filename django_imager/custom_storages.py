2b82f7be2c8638afe4f47721876a02efc9bb3600
Chris Closser <chrisclosser1@gmail.com>
2018-01-02 14:12:07 -0800

    travis

--- django_imager/custom_storages.py
+++ django_imager/custom_storages.py
@@ -1,14 +1,14 @@
-"""Custom storage classes for static and media files."""
-from __future__ import absolute_import
-from django.conf import settings
-from storages.backends.s3boto import S3BotoStorage
-
-class StaticStorage(S3BotoStorage):
-    """Custom storage class for static files."""
-
-    location = settings.STATICFILES_LOCATION
-
-class MediaStorage(S3BotoStorage):
-    """Custom storage class for media files."""
-
-    location = settings.MEDIAFILES_LOCATION
+# """Custom storage classes for static and media files."""
+# from __future__ import absolute_import
+# from django.conf import settings
+# from storages.backends.s3boto import S3BotoStorage
+#
+# class StaticStorage(S3BotoStorage):
+#     """Custom storage class for static files."""
+#
+#     location = settings.STATICFILES_LOCATION
+#
+# class MediaStorage(S3BotoStorage):
+#     """Custom storage class for media files."""
+#
+#     location = settings.MEDIAFILES_LOCATION
